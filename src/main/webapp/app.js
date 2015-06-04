499ba6ca1513cc83f80a27829bc436c4b23b02a9
Daniel Yinanc <dyinanc@localhost.localdomain>
2015-06-04 16:14:24 -0500

    Gitignore corrected and first version of Ext added

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,16 +1,56 @@
-Ext.application({
-    name   : 'MyApp',
+Ext.define('MyApp.view.TestViewModel', {
+    extend: 'Ext.app.ViewModel',
 
-    launch : function() {
+    alias: 'viewmodel.test', // connects to viewModel/type below
 
-        Ext.create('Ext.Panel', {
-            renderTo     : Ext.getBody(),
-            width        : 200,
-            height       : 150,
-            bodyPadding  : 5,
-            title        : 'Hello World',
-            html         : 'Hello <b>World</b>...'
-        });
+    data: {
+        firstName: 'John',
+        lastName: 'Doe'
+    },
 
+    formulas: {
+        // We'll explain formulas in more detail soon.
+        name: function (get) {
+            var fn = get('firstName'), ln = get('lastName');
+            return (fn && ln) ? (fn + ' ' + ln) : (fn || ln || '');
+        }
     }
+});
+
+Ext.define('MyApp.view.TestView', {
+    extend: 'Ext.panel.Panel',
+    layout: 'form',
+
+    // Always use this form when defining a view class. This
+    // allows the creator of the component to pass data without
+    // erasing the ViewModel type that we want.
+    viewModel: {
+        type: 'test'  // references alias "viewmodel.test"
+    },
+
+    bind: {
+        title: 'Hello {name}'
+    },
+
+    defaultType: 'textfield',
+    items: [{
+        fieldLabel: 'First Name',
+        bind: '{firstName}'
+    },{
+        fieldLabel: 'Last Name',
+        bind: '{lastName}'
+    },{
+        xtype: 'button',
+        text: 'Submit',
+        bind: {
+            hidden: '{!name}'
+        }
+    }]
+});
+
+Ext.onReady(function () {
+    Ext.create('MyApp.view.TestView', {
+        renderTo: Ext.getBody(),
+        width: 400
+    });
 });
